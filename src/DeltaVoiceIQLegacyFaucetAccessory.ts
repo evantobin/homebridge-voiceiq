5a70fee730e0187b3a0befa71073208fd97264f2
Evan Tobin <me@evantob.in>
2023-01-04 00:37:52 -0600

    Fix issue with plugin not updating status Changed type to valve because I think the homebridge faucet is just borked

--- src/DeltaVoiceIQLegacyFaucetAccessory.ts
+++ src/DeltaVoiceIQLegacyFaucetAccessory.ts
@@ -26,31 +26,23 @@ export class DeltaVoiceIQLegacyFaucetAccessory {
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Delta')
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Delta Faucet')
       .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.productId)
       .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.macAddress);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Faucet) || this.accessory.addService(this.platform.Service.Faucet);
-
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service = this.accessory.getService(this.platform.Service.Valve) || this.accessory.addService(this.platform.Service.Valve);
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+    this.service.setCharacteristic(this.platform.Characteristic.ValveType, this.platform.Characteristic.ValveType.WATER_FAUCET);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.Active)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
+
+    this.service.getCharacteristic(this.platform.Characteristic.InUse)
+      .onGet(this.getOn.bind(this));
+
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
   async setOn(value: CharacteristicValue) {
     // implement your own code to turn your device on/off
     this.state.On = value as boolean;
@@ -69,21 +61,7 @@ export class DeltaVoiceIQLegacyFaucetAccessory {
     this.platform.log.debug('Set Characteristic On ->', value);
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
+  getOn(): CharacteristicValue {
     const isOn = this.state.On ? this.platform.Characteristic.Active.ACTIVE : this.platform.Characteristic.Active.INACTIVE;
 
     this.platform.log.debug('Get Characteristic On ->', isOn);
